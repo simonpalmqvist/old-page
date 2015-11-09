46d441e8784ef8d2254d903ee56dba7e83200e98
Simon Palmqvist <simon.palmqvist@gmail.com>
2015-11-09 12:51:18 +0100

    Adding so footer is always in the bottom of the page and fixing issue with mobile menu being under page content.

---
+++ assets/js/load.js
@@ -0,0 +1,8 @@
+$(window).load(function() {
+    var windowHeight = $(window).height();
+    var bodyHeight = $("body").height();
+
+    if ( bodyHeight < windowHeight) {
+        $("footer").css("margin-top", windowHeight - bodyHeight);
+    }
+});
