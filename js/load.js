8d6df9ffb5406b2612145ac0396b6b415bd91296
Simon Palmqvist <simon.palmqvist@gmail.com>
2015-11-10 12:42:48 +0100

    Adding disqus for blogposts.

--- js/load.js
+++ js/load.js
@@ -1,14 +1,44 @@
 ---
 ---
 
-$(window).load(function() {
+function loadDiscuss() {
+    var disqus_config = function () {
+        var disqus_developer = 1;
+        this.page.shortname = "sp222kh";
+        this.page.identifier = "{{ page.url }}";
+    };
 
+    (function() { // DON'T EDIT BELOW THIS LINE
+        var d = document, s = d.createElement('script');
+
+        s.src = '//simonpalmqvist.disqus.com/embed.js';
+
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+
+    })();
+};
+
+function moveFooter() {
     var windowHeight = $(window).height();
     var bodyHeight = $("body").height();
 
     if ( bodyHeight < windowHeight) {
-        $("footer").css("margin-top", windowHeight - bodyHeight);
+        $(".site-footer").css("margin-top", windowHeight - bodyHeight);
+    } else if (bodyHeight > windowHeight) {
+        $(".site-footer").css("margin-top", 0);
     }
+}
+
+
+$(window).load(function() {
+    if ($("#disqus_thread").length) {
+        loadDiscuss();
+        $(".post-footer").bind("DOMSubtreeModified", function() {
+            moveFooter();
+        });
+    }
+
+    moveFooter();
 });
 
-console.log("what");
